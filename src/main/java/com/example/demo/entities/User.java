20ac1f4bc6ed464ba241b9f6c90875a7f4c1b2ee
Julia.Mek <juliaa.mekarska@gmail.com>
2025-08-22 08:07:08 +0000

    Added data base fix

--- src/main/java/com/example/demo/entities/User.java
+++ src/main/java/com/example/demo/entities/User.java
@@ -18,7 +18,7 @@ public class User {
     private List<PortfolioAsset> portfolioAssets;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
-    private List<Transaction> transactions;
+    private List<UserTransaction> transactions;
 
     // gettery/settery
 }
